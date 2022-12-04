9c12092205f7b8c069620e70cb8342731885112e
Yeon-junLee <83509116+Yeon-junLee@users.noreply.github.com>
2022-12-04 18:54:21 +0900

    으아아아아ㅏ

--- Assets/Scripts/Firefireball.cs
+++ Assets/Scripts/Firefireball.cs
@@ -7,9 +7,18 @@ public class Firefireball : MonoBehaviour
     public GameObject Fireball_red;
     public GameObject Fireball_blue;
     public GameObject Fireball_turquoise;
+
+    private ChooseManager choosemanager = null;
+
+    private Vector3 touchedPos;
+
+    public Camera cam;
+
+    private float power = 5.0f;
     void Start()
     {
-        
+        GameObject cmObject = GameObject.Find("ChooseMangaer");
+        choosemanager = cmObject.GetComponent<ChooseManager>();
     }
 
     // Update is called once per frame
@@ -22,6 +31,34 @@ public class Firefireball : MonoBehaviour
         if (touch.phase != TouchPhase.Began)
             return;
 
-        
+        touchedPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+        if (choosemanager.Fire_red == false && choosemanager.Fire_blue == false && choosemanager.Fire_tur == false)
+            return;
+
+        if(choosemanager.Fire_red == true)
+        {
+            GameObject fireball_red = Instantiate(Fireball_red);
+            fireball_red.transform.position = touchedPos;
+
+            Vector3 direction = cam.transform.localRotation * Vector3.forward + new Vector3(0.0f, 1.5f, 0.0f);
+            fireball_red.GetComponent<Rigidbody>().velocity = direction * power;
+        }
+        else if (choosemanager.Fire_blue == true)
+        {
+            GameObject fireball_blue = Instantiate(Fireball_blue);
+            fireball_blue.transform.position = touchedPos;
+
+            Vector3 direction = cam.transform.localRotation * Vector3.forward + new Vector3(0.0f, 1.5f, 0.0f);
+            fireball_blue.GetComponent<Rigidbody>().velocity = direction * power;
+        }
+        else if (choosemanager.Fire_blue == true)
+        {
+            GameObject fireball_tur = Instantiate(Fireball_turquoise);
+            fireball_tur.transform.position = touchedPos;
+
+            Vector3 direction = cam.transform.localRotation * Vector3.forward + new Vector3(0.0f, 1.5f, 0.0f);
+            fireball_tur.GetComponent<Rigidbody>().velocity = direction * power;
+        }
     }
 }
